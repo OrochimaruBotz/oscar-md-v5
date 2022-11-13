34739d68d81bfb439b2a6f1f0b4b4160200ee567
Humanz <109642842+OrochimaruBotz@users.noreply.github.com>
2022-11-13 22:49:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -84,8 +84,8 @@ global.fsizedoc = '564588643' // default 10TB
 global.fpagedoc = '444'
 
 // Sticker WM
-global.stickpack = 'Sticker By'
-global.stickauth = 'HumanzzZ'
+global.packname = 'Sticker By'
+global.author = 'HumanzzZ'
 global.wm = '© HumanzzZ'
 
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
